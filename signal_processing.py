22268a1df98049338b60b25043a78bb84af5ee5b
Aki Kunikoshi <akikun@tesla01.uppsala.readspeaker.net>
2020-11-02 11:43:08 +0000

    trim_silB_silE is added.

--- signal_processing.py
+++ signal_processing.py
@@ -17,7 +17,39 @@ import scipy
 #             os.path.basename(wav_file_out), sample_rate_original, sample_rate))
         
         
-def change_sample_bit_rate(wav_file_in, wav_file_out, sample_rate=22050, bit_rate=16, channel=1):
+def change_sample_bit_rate(wav_path_in, wav_path_out, sample_rate=22050, bit_rate=16, channel=1):
     ''' default setting is for waveglow_vocoder '''
-    command = 'sox ' + wav_file_in + ' -r ' + str(sample_rate) + ' -b ' + str(bit_rate) + ' -c ' + str(channel) + ' ' + wav_file_out
+    command = ('sox ' + wav_path_in 
+                + ' -r ' + str(sample_rate) 
+                + ' -b ' + str(bit_rate) 
+                + ' -c ' + str(channel) 
+                + ' ' + wav_path_out)
+    os.system(command)
+
+
+def trim_silB_silE(wav_path_in, wav_path_out, duration_threshold=1, volume_threshold=0.1):
+    '''
+        trim silB (silence - begin) and silE (silence - end)
+    
+        Args:
+            wav_path_in: wav file to be trimmed.
+            
+            wav_path_out: wav file which is trimmed.
+            
+            duration_threshold: trim silence (anything less than {volume_threshold}%) 
+                until we encounter sound lasting more than {duration_threshold} seconds in duration. 
+                default value = 1
+                
+            volume_threshold: used to indicate what sample value you should treat as silence. 
+                For digital audio, a value of 0 may be fine but for audio recorded from analog, 
+                you may wish to increase the value to account for background noise.
+                default value = 0.1
+                
+    '''
+    command = ('sox ' + wav_path_in + ' ' + wav_path_out
+                + ' silence 1 '
+                + str(duration_threshold) + ' ' + str(volume_threshold)
+                + '% reverse silence 1 '
+                + str(duration_threshold) + ' ' + str(volume_threshold) 
+                + '% reverse')
     os.system(command)
